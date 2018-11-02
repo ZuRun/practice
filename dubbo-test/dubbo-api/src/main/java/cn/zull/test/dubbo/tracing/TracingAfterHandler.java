cd9d6baad2326c3d670dc45fd31f81c667c62915
ZuRun's MacBook Pro <hi@zuhr.me>
2018-11-02 23:49:49 +0800

    -

---
+++ dubbo-test/dubbo-api/src/main/java/cn/zull/test/dubbo/tracing/TracingAfterHandler.java
@@ -0,0 +1,21 @@
+package cn.zull.test.dubbo.tracing;
+
+import cn.zull.tracing.core.after.TracingLogHandler;
+import cn.zull.tracing.core.model.TraceLog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zurun
+ * @date 2018/11/2 15:25:01
+ */
+@Component
+public class TracingAfterHandler implements TracingLogHandler {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void handler(TraceLog traceLog) {
+        logger.warn("【链路】" + traceLog.toString());
+    }
+}
---
+++ kafka-learning/src/main/java/cn/zull/practice/test/kafka/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author zurun
+ * @date 2018/10/26 14:55:35
+ */
+package cn.zull.practice.test.kafka;
