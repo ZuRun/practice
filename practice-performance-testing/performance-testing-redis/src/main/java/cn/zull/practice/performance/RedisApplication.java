19d4d7a212135914f172e67e2f004a7a4041fd88
ZuRun's MacBook Pro <hi@zull.cn>
2019-10-15 17:26:29 +0800

    redis压测

---
+++ practice-performance-testing/performance-testing-redis/src/main/java/cn/zull/practice/performance/RedisApplication.java
@@ -0,0 +1,15 @@
+package cn.zull.practice.performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zurun
+ * @date 2019/10/15 17:25:10
+ */
+@SpringBootApplication
+public class RedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
---
+++ practice-performance-testing/performance-testing-redis/src/main/java/cn/zull/practice/performance/redis/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author zurun
+ * @date 2019/10/15 17:15:26
+ */
+package cn.zull.practice.performance.redis;
